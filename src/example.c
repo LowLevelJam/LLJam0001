a36d88205bdcfa3ff39eec0f434ba86b1a466222
Max Starr <max@maxstarr.xyz>
2022-06-17 21:22:32 -0500

    initial playing around

--- src/example.c
+++ src/example.c
@@ -1,22 +1,22 @@
 /******************************************************************************
-
+ 
  Copyright (c) 2015, Focusrite Audio Engineering Ltd.
  All rights reserved.
-
+ 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
-
+ 
  * Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
-
+ 
  * Redistributions in binary form must reproduce the above copyright notice,
  this list of conditions and the following disclaimer in the documentation
  and/or other materials provided with the distribution.
-
+ 
  * Neither the name of Focusrite Audio Engineering Ltd., nor the names of its
  contributors may be used to endorse or promote products derived from
  this software without specific prior written permission.
-
+ 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -27,7 +27,7 @@
  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+ 
  *****************************************************************************/
 
 //______________________________________________________________________________
@@ -58,61 +58,151 @@ u8 g_Buttons[BUTTON_COUNT] = {0};
 
 void app_surface_event(u8 type, u8 index, u8 value)
 {
-  switch (type)
-  {
-  case TYPEPAD:
-  {
-    // toggle it and store it off, so we can save to flash if we want to
-    if (value)
+    switch (type)
     {
-      g_Buttons[index] = MAXLED * !g_Buttons[index];
+        case  TYPEPAD:
+        {
+            // toggle it and store it off, so we can save to flash if we want to
+            if (value)
+            {
+                g_Buttons[index] = MAXLED * !g_Buttons[index];
+            }
+            
+            // example - light / extinguish pad LEDs
+            hal_plot_led(TYPEPAD, index, 0, 0, g_Buttons[index]);
+            
+            // example - send MIDI
+            hal_send_midi(DINMIDI, NOTEON | 0, index, value);
+            
+        }
+        break;
+            
+        case TYPESETUP:
+        {
+            if (value)
+            {
+                // save button states to flash (reload them by power cycling the hardware!)
+                hal_write_flash(0, g_Buttons, BUTTON_COUNT);
+            }
+        }
+        break;
     }
-
-    // example - light / extinguish pad LEDs
-    hal_plot_led(TYPEPAD, index, 0, 0, g_Buttons[index]);
-    break;
-  }
-  }
 }
 
 //______________________________________________________________________________
 
-void app_midi_event(u8 port, u8 status, u8 d1, u8 d2) {}
+void app_midi_event(u8 port, u8 status, u8 d1, u8 d2)
+{
+    // example - MIDI interface functionality for USB "MIDI" port -> DIN port
+    if (port == USBMIDI)
+    {
+        hal_send_midi(DINMIDI, status, d1, d2);
+    }
+    
+    // // example -MIDI interface functionality for DIN -> USB "MIDI" port port
+    if (port == DINMIDI)
+    {
+        hal_send_midi(USBMIDI, status, d1, d2);
+    }
+}
 
 //______________________________________________________________________________
 
-void app_sysex_event(u8 port, u8 *data, u16 count) {}
+void app_sysex_event(u8 port, u8 * data, u16 count)
+{
+    // example - respond to UDI messages?
+}
 
 //______________________________________________________________________________
 
-void app_aftertouch_event(u8 index, u8 value) {}
+void app_aftertouch_event(u8 index, u8 value)
+{
+    // example - send poly aftertouch to MIDI ports
+    hal_send_midi(USBMIDI, POLYAFTERTOUCH | 0, index, value);
+    
+    
+}
 
 //______________________________________________________________________________
 
-void app_cable_event(u8 type, u8 value) {}
+void app_cable_event(u8 type, u8 value)
+{
+    // example - light the Setup LED to indicate cable connections
+    if (type == MIDI_IN_CABLE)
+    {
+        hal_plot_led(TYPESETUP, 0, 0, value, 0); // green
+    }
+    else if (type == MIDI_OUT_CABLE)
+    {
+        hal_plot_led(TYPESETUP, 0, value, 0, 0); // red
+    }
+}
 
 //______________________________________________________________________________
 
-void app_timer_event() {}
+void app_timer_event()
+{
+    // example - send MIDI clock at 125bpm
+#define TICK_MS 20
+    
+    static u8 ms = TICK_MS;
+    
+    if (++ms >= TICK_MS)
+    {
+        ms = 0;
+        
+        // send a clock pulse up the USB
+        hal_send_midi(USBSTANDALONE, MIDITIMINGCLOCK, 0, 0);
+    }
+    
+	// alternative example - show raw ADC data as LEDs
+	for (int i=0; i < PAD_COUNT; ++i)
+	{
+		// raw adc values are 12 bit, but LEDs are 6 bit.
+		// Let's saturate into r;g;b for a rainbow effect to show pressure
+		u16 r = 0;
+		u16 g = 0;
+		u16 b = 0;
+		
+		u16 x = (3 * MAXLED * g_ADC[i]) >> 12;
+		
+		if (x < MAXLED)
+		{
+			r = x;
+		}
+		else if (x >= MAXLED && x < (2*MAXLED))
+		{
+			r = 2*MAXLED - x;
+			g = x - MAXLED;
+		}
+		else
+		{
+			g = 3*MAXLED - x;
+			b = x - 2*MAXLED;
+		}
+		
+		hal_plot_led(TYPEPAD, ADC_MAP[i], r, g, b);
+	}
+}
 
 //______________________________________________________________________________
 
 void app_init(const u16 *adc_raw)
 {
-  // example - load button statess from flash
-  // hal_read_flash(0, g_Buttons, BsUTTON_COUNT);
-
-  // example - light the LEDs to say hello!
-  for (int i = 0; i < 10; ++i)
-  {
-    for (int j = 0; j < 10; ++j)
+    // example - load button states from flash
+    hal_read_flash(0, g_Buttons, BUTTON_COUNT);
+    
+    // example - light the LEDs to say hello!
+    for (int i=0; i < 10; ++i)
     {
-      u8 b = g_Buttons[j * 10 + i];
-
-      hal_plot_led(TYPEPAD, j * 10 + i, 0, 0, b);
+        for (int j=0; j < 10; ++j)
+        {
+            u8 b = g_Buttons[j*10 + i];
+            
+            hal_plot_led(TYPEPAD, j*10 + i, 0, 0, b);
+        }
     }
-  }
-
-  // store off the raw ADC frame pointer for later use
-  g_ADC = adc_raw;
+	
+	// store off the raw ADC frame pointer for later use
+	g_ADC = adc_raw;
 }

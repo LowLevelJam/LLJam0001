cd4c1a94bb6c60a38c5895e005765f7ef966c6bd
Max Starr <max@maxstarr.xyz>
2022-06-20 22:47:29 -0500

    comments and cleanup

--- src/app.c
+++ src/app.c
@@ -1,53 +1,6 @@
-/******************************************************************************
 
- Copyright (c) 2015, Focusrite Audio Engineering Ltd.
- All rights reserved.
-
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
-
- * Redistributions of source code must retain the above copyright notice, this
- list of conditions and the following disclaimer.
-
- * Redistributions in binary form must reproduce the above copyright notice,
- this list of conditions and the following disclaimer in the documentation
- and/or other materials provided with the distribution.
-
- * Neither the name of Focusrite Audio Engineering Ltd., nor the names of its
- contributors may be used to endorse or promote products derived from
- this software without specific prior written permission.
-
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
- *****************************************************************************/
-
-//______________________________________________________________________________
-//
-// Headers
-//______________________________________________________________________________
 
 #include "app.h"
-
-//______________________________________________________________________________
-//
-// This is where the fun is!  Add your code to the callbacks below to define how
-// your app behaves.
-//
-// In this example, we either render the raw ADC data as LED rainbows or store
-// and recall the pad state from flash.
-//______________________________________________________________________________
-
-// store ADC frame pointer
-// static const u16 *g_ADC = 0;
 
 // buffer to store pad states for flash save
 #define BUTTON_COUNT 100
@@ -92,26 +45,36 @@ u8 held_button_index = 0;
 
 //______________________________________________________________________________
 
+// This function is kindof insane.
+// It just checks if the supplied index is one of the allowed side buttons
+// For the user input, clocks, gates, and output routing.
+// This could be compacted but it works fine and I don't feel like changing it. c:
 u8 is_input_button(u8 index)
 {
-  // TODO: check if this is an input button
-  return index == 10 || (index > 90 && index < 97) || (index > 0 && index < 9) || index == 19 || index == 29 || index == 39 || index == 49 || index == 59 || index == 69 || index == 79 || index == 89;
+  return (index > 90 && index < 97) || (index > 0 && index < 11) || index == 19 || index == 29 || index == 39 || index == 49 || index == 59 || index == 69 || index == 79 || index == 89;
 }
 
 // user input buttons range from 1 to 8
+// These are the bottom row of buttons
 u8 is_user_input_button(u8 index)
 {
   return index > 0 && index < 9;
 }
 
+// This is the callback that's triggered when a button is pressed
 void app_surface_event(u8 type, u8 index, u8 value)
 {
+  // The switch here could just be an if because we don't care about the other case just yet
+  // But it was here from the example code so I kept it
   switch (type)
   {
   case TYPEPAD:
   {
+    // Here's where that insane function from before comes in
     if (is_input_button(index))
     {
+      // We check if value to know if the button is being pressed or released
+      // Value will be 0 if it's released and some positive number if it's being pressed
       if (value)
       {
         held_button_index = index;
@@ -131,6 +94,8 @@ void app_surface_event(u8 type, u8 index, u8 value)
     // anding with value ensures this only triggers on a down press
     if (held_button_index && value)
     {
+      // If the button already has the same input index, we want to remove it
+      // This is the only way to 'unset' a button
       if (display_state[index].input_index == held_button_index)
       {
         display_state[index].input_index = 0;
@@ -139,6 +104,8 @@ void app_surface_event(u8 type, u8 index, u8 value)
       }
       else
       {
+        // Check for the gate type
+        // This could be done with a lookup table later
         switch (held_button_index)
         {
 
@@ -203,6 +170,9 @@ void app_surface_event(u8 type, u8 index, u8 value)
         }
         default:
         {
+          // This case handles both user input and the clock
+          // Since both have their output set external to the simulation
+          // They can be treated as input targets
           display_state[index].input_index = held_button_index;
           display_state[index].type = INPUT;
           hal_plot_led(TYPEPAD, index, 0, MAXLED, 0);
@@ -216,6 +186,8 @@ void app_surface_event(u8 type, u8 index, u8 value)
   }
 }
 
+// None of these callbacks are used
+// But the compiler complains if the stubs aren't here
 //______________________________________________________________________________
 
 void app_midi_event(u8 port, u8 status, u8 d1, u8 d2) {}
@@ -234,6 +206,11 @@ void app_cable_event(u8 type, u8 value) {}
 
 //______________________________________________________________________________
 
+// This is the main loop of the application
+// The documentation would have me believe it's run every millisecond
+// Though I don't know how accurate the timing is
+// This is where the simulation is done
+// And where the lights are updated
 void app_timer_event()
 {
 #define TICK_MS 500
@@ -306,31 +283,12 @@ void app_timer_event()
 
 void app_init(const u16 *adc_raw)
 {
-  // example - load button statess from flash
-  // hal_read_flash(0, g_Buttons, BsUTTON_COUNT);
-
+  // This just sets the color coding for the gate select buttons at the top.
+  // The LEDs are static so these values don't need to be refreshed
   hal_plot_led(TYPEPAD, 91, MAXLED, 0, MAXLED);
   hal_plot_led(TYPEPAD, 92, 0, MAXLED, 0);
   hal_plot_led(TYPEPAD, 93, MAXLED, 0, 0);
   hal_plot_led(TYPEPAD, 94, 0, MAXLED, MAXLED);
   hal_plot_led(TYPEPAD, 95, MAXLED, MAXLED, 0);
   hal_plot_led(TYPEPAD, 96, MAXLED, MAXLED, MAXLED);
-
-  // example - light the LEDs to say hello !
-
-  // for (int i = 1; i < 9; ++i)
-  // {
-  //   for (int j = 1; j < 9; ++j)
-  //   {
-  //     // u8 b = g_Buttons[j * 10 + i];
-
-  //     temp_state[j * 10 + i] = does_cell_live(i, j);
-  //     hal_plot_led(TYPEPAD, j * 10 + i, 0, 0, temp_state[j * 10 + i] * MAXLED);
-  //   }
-  // }
-
-  // memcpy(display_state, temp_state, sizeof(display_state));
-
-  // store off the raw ADC frame pointer for later use
-  // g_ADC = adc_raw;
 }
